8b5ab72c65ff0de24e259b9b90d5853577de5cb0
prashkup <prashanthkuppa333@gmail.com>
2019-04-12 15:34:22 +0530

    Bootstrap the gui

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import * as gui from "gui"
+import Expression from "./Entities/Expression"
+import UpdateExpression from "./UseCases/UpdateExpression/UpdateExpression"
+import Presenter from "./Boundaries/Presenter"
+import Gui from "./infrastructure/CalculatorUI"
+import Controller from "./Boundaries/Controller"
+import Keypad from "./infrastructure/Keypad"
+import Display from "./infrastructure/Display"
 
-const YueWindow = gui.Window.create({})
+const display = new Display()
+const expression = new Expression()
+const presenter  = new Presenter(display)
+const updateExpression = new UpdateExpression (expression, presenter)
+const controller = new Controller(updateExpression)
+const keypad = new Keypad(controller)
 
-YueWindow.activate()
-YueWindow.onClose = () => gui.MessageLoop.quit()
+const gui = new Gui(keypad, display)
 
-gui.MessageLoop.run()
-process.exit(0)
+gui.showGui()
