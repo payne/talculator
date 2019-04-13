8c4161f5e69d4824a27186a2640cd619bd71f8f0
prashkup <prashanthkuppa333@gmail.com>
2019-04-13 16:12:13 +0530

    Rename getKeyPad to getKeypadView

--- src/infrastructure/Keypad.ts
+++ src/infrastructure/Keypad.ts
@@ -8,7 +8,7 @@ class Keypad {
     this.controller = controller
   }
 
-  getKeyPad (): View[] {
+  getKeypadView (): View[] {
     let digits = ["0","1","2","3","4","5","6","7","8","9"]
     let operators = ["+", "-", "/", "x", "="]
     let otherKeys = [".","AC","CE"]
