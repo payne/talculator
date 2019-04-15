d89fdb403beca7e6940462200a9e3356225885d3
prashkup <prashanthkuppa333@gmail.com>
2019-04-15 19:01:34 +0530

    Remove unneeded test cases

--- src/Entities/Expression.test.ts
+++ src/Entities/Expression.test.ts
@@ -11,13 +11,7 @@ test("isNumber should return whether the expression is a number or not", (t) => 
 
   expression.setValue("26 - 3")
   t.false(expression.isNumber(), "26 - 3 is not a number")
-
-  expression.setValue("")
-  t.false(expression.isNumber(), "an empty expression is not a number")
-
-  expression.setValue("+")
-  t.false(expression.isNumber(), "+ is not a number")
-
+  
   t.end()
 })
 
@@ -30,12 +24,6 @@ test("getLastNumber should return the last number from the expression", (t) => {
 
   expression.setValue("22 + 33")
   t.equal(expression.getLastNumber(), "33")
-
-  expression.setValue("22 * 3 + - /")
-  t.equal(expression.getLastNumber(), "3")
-
-  expression.setValue("+")
-  t.equal(expression.getLastNumber(), undefined)
 
   t.end()
 })
