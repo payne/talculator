76427a46397b7de64ac4a0f7b35e0979c799e0b6
prashkup <prashanthkuppa333@gmail.com>
2019-04-28 22:44:39 +0530

    Sort imports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
+// infrastructure layer
 import Gui from './infrastructure/CalculatorUI'
 import Display from './infrastructure/Display'
 import Keypad from './infrastructure/Keypad'
-import ExpressionUpdaterController from './Boundaries/ExpressionUpdater/ExpressionUpdaterController'
-import CalculatorController from './Boundaries/Calculator/CalculatorController'
-import Presenter from './Boundaries/Presenter'
-import Expression from './Entities/Expression'
-import ExpressionUpdater from './UseCases/ExpressionUpdater/ExpressionUpdater'
-import Calculator from './UseCases/Calculator/Calculator'
 
-const display = new Display()
+// boundary layer
+import CalculatorController from './Boundaries/Calculator/CalculatorController'
+import ExpressionUpdaterController from './Boundaries/ExpressionUpdater/ExpressionUpdaterController'
+import Presenter from './Boundaries/Presenter'
+
+// use-case layer
+import Calculator from './UseCases/Calculator/Calculator'
+import ExpressionUpdater from './UseCases/ExpressionUpdater/ExpressionUpdater'
+
+// entity layer
+import Expression from './Entities/Expression'
+
 const expression = new Expression()
+const display = new Display()
 const presenter  = new Presenter(display)
 const expressionUpdater = new ExpressionUpdater(expression, presenter)
 const calculator = new Calculator(expression, presenter)
