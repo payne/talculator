bde3948695851e1de7bad28defee9a29ade4e774
prashkup <prashanthkuppa333@gmail.com>
2019-04-28 22:38:28 +0530

    Inject the dependencies for all the components

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import Gui from './infrastructure/CalculatorUI'
 import Display from './infrastructure/Display'
 import Keypad from './infrastructure/Keypad'
-import Controller from './Boundaries/ExpressionUpdater/ExpressionUpdaterController'
+import ExpressionUpdaterController from './Boundaries/ExpressionUpdater/ExpressionUpdaterController'
+import CalculatorController from './Boundaries/Calculator/CalculatorController'
 import Presenter from './Boundaries/Presenter'
 import Expression from './Entities/Expression'
 import ExpressionUpdater from './UseCases/ExpressionUpdater/ExpressionUpdater'
+import Calculator from './UseCases/Calculator/Calculator'
 
 const display = new Display()
 const expression = new Expression()
 const presenter  = new Presenter(display)
 const expressionUpdater = new ExpressionUpdater(expression, presenter)
-const controller = new Controller(expressionUpdater)
-const keypad = new Keypad(controller)
+const calculator = new Calculator(expression, presenter)
+const expressionUpdaterController = new ExpressionUpdaterController(expressionUpdater)
+const calculatorController = new CalculatorController(calculator)
+const keypad = new Keypad(expressionUpdaterController, calculatorController)
 
 const gui = new Gui(keypad, display)
 
