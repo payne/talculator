5237b876177f22a7d1d0b8eae8047ef77b018fe9
prashkup <prashanthkuppa333@gmail.com>
2019-05-05 22:16:40 +0530

    Add a Data transfer object to receive input

--- src/Boundaries/ExpressionUpdater/ExpressionUpdaterController.ts
+++ src/Boundaries/ExpressionUpdater/ExpressionUpdaterController.ts
@@ -1,6 +1,7 @@
 import IExpressionUpdaterInputPort from '../../UseCases/ExpressionUpdater/InputPort/IExpressionUpdaterInputPort'
-import {IExpressionUpdaterInputData} from '../../UseCases/ExpressionUpdater/InputPort/IExpressionUpdaterInputPort'
+import {IExpressionUpdaterRequestModel} from '../../UseCases/ExpressionUpdater/InputPort/IExpressionUpdaterInputPort'
 import IRequest from './InputPort/IExpressionUpdaterRequest'
+import {IExpressionUpdaterDTO} from './InputPort/IExpressionUpdaterRequest'
 
 class Controller implements IRequest {
   private inputPort: IExpressionUpdaterInputPort
@@ -9,8 +10,8 @@ class Controller implements IRequest {
     this.inputPort = inputPort
   }
 
-  public updateExpression (inputData: IExpressionUpdaterInputData) {
-    this.inputPort.updateExpression(inputData)
+  public updateExpression (inputData: IExpressionUpdaterDTO) {
+    this.inputPort.updateExpression(inputData as IExpressionUpdaterRequestModel)
   }
 }
 
--- src/Boundaries/ExpressionUpdater/InputPort/IExpressionUpdaterRequest.ts
+++ src/Boundaries/ExpressionUpdater/InputPort/IExpressionUpdaterRequest.ts
@@ -1,5 +1,12 @@
-interface IRequest {
+// tslint:disable-next-line: max-line-length
+type valueType = 'digit' | 'operator' | 'decimal' | 'clearLastValue' | 'clearExpression'
+export interface IExpressionUpdaterDTO {
+  value: string,
+  valueType: valueType
+}
+
+interface IExpressionUpdaterRequest {
   updateExpression (value: any): void
 }
 
-export default IRequest
+export default IExpressionUpdaterRequest
